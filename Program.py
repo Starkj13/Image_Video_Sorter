ef2e35a8cc80a8d0749054815686d0ad61b7d5c8
Albin K <87191869+Starkj13@users.noreply.github.com>
2024-02-12 18:08:20 +0100

    Change

--- Program.py
+++ Program.py
@@ -19,7 +19,7 @@ def separate_images_and_videos(root_folder, image_folder, video_folder):
                     shutil.move(file_path, os.path.join(video_folder, filename))
 
 # Input folders from the user
-root_folder = input("Enter the root folder path: ")
+root_folder = input("Destenation folder: ")
 image_folder = input("Enter the image folder path: ")
 video_folder = input("Enter the video folder path: ")
 
